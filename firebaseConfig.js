cb8620fe33ba26a4ea2b20dd0062f4b9a32b8243
IsuruG99 <isuru9917@gmail.com>
2025-06-14 03:53:25 +0530

    Initial Commit.

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJkYw101oulVN4SGaTpznXNmcvwSN6cZM",
+    authDomain: "thisara-travels.firebaseapp.com",
+    databaseURL: "https://thisara-travels-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "thisara-travels",
+    storageBucket: "thisara-travels.firebasestorage.app",
+    messagingSenderId: "704254905571",
+    appId: "1:704254905571:web:e5645ab032bd5b246fc9d6"
+};
+
+// Initialize Firebase
+window.app = firebase.initializeApp(firebaseConfig);
